c6d93963ee4aa7444ecd0726cb612dc9e2ab1cb9
Ihor <moskigo2@gmail.com>
2016-12-04 18:08:40 +0200

    addMethodMinus

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -13,10 +13,16 @@ namespace TestGit
             Console.WriteLine("Sum= "+(a+b));
         }
 
+        public void Minus(double a, double b)
+        {
+            Console.WriteLine("Result= "+(a-b));
+        }
+
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Sum(7, 8);
+            p.Minus(23, 6);
         }
     }
 }
