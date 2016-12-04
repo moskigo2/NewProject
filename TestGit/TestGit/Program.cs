092d17ee13af3289a58430257b22ffbef35f5e5e
Ihor <moskigo2@gmail.com>
2016-12-04 17:58:39 +0200

    addProject

---
+++ TestGit/TestGit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    class Program
+    {
+        public void Sum(double a, double b)
+        {
+            Console.WriteLine("Sum= "+(a+b));
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Sum(7, 8);
+        }
+    }
+}
