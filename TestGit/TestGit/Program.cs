1df58b5c2d043a842bba2d4a7a5a68ae59c243b2
Ihor <moskigo2@gmail.com>
2016-12-04 18:18:45 +0200

    addMethodDivide

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -13,10 +13,18 @@ namespace TestGit
             Console.WriteLine("Sum= "+(a+b));
         }
 
+        public void Divide(double a, double b)
+        {
+            if (b != 0)
+                Console.WriteLine("Devide result= " + (a / b));
+            else throw new DivideByZeroException();
+        }
+
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Sum(7, 8);
+            
         }
     }
 }
